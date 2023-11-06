f0c666663edda79e5429e21223d7aaf872646e8e
Bryan Suárez <bryan@taxo.co>
2023-11-05 22:46:02 -0500

    create and listing topics in a component

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\TopicResource;
+use App\Models\Topic;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
@@ -39,6 +41,7 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
+            'topics' => TopicResource::collection(Topic::orderBy('title', 'asc')->get()),
         ];
     }
 }
--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import ForumLayout from "@/Layouts/ForumLayout.vue";
+import Select from "@/Components/Select.vue";
+import InputLabel from "@/Components/InputLabel.vue";
 
 import { Head } from "@inertiajs/vue3";
 </script>
@@ -9,7 +11,19 @@ import { Head } from "@inertiajs/vue3";
 
     <ForumLayout>
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">Content</div>
+            <div class="p-6 text-gray-900">
+                <InputLabel for="topic" value="Temas" class="sr-only" />
+                <Select name="topics" id="topic">
+                    <option value="">Todos los temas</option>
+                    <option
+                        :value="topic.title"
+                        v-for="topic in $page.props.topics"
+                        :key="topic.id"
+                    >
+                        {{ topic.title }}
+                    </option>
+                </Select>
+            </div>
         </div>
 
         <template #side>
